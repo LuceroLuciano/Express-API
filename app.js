f8c7063eca6602f34485a5fd6f728a22891de187
LuceroLuciano <lucero.luciano@umb.edu.mx>
2022-05-03 17:57:52 -0500

    Creando un endpoint que regrese un explorer mediante ID

--- app.js
+++ app.js
@@ -16,6 +16,14 @@ app.get('/v1/explorers', (req, res) => {
     const explorers = [explorer1, explorer2, explorer3, explorer4]
     res.status(200).json(explorers)
 })
+
+app.get('/v1/explorers/:id', (req, res) => {
+    console.log(`Api Explorers GET request ${new Date()}`)
+    console.log(`Getting explorer with id ${req.params.id}`)
+    const explorer = {id: 1, name: "Lucero"}
+    res.status(200).json(explorer)
+})
+
 // Con esto inicializamos esta app
 app.listen(port, () => {
     console.log(`Example app listen on port ${port}`)
